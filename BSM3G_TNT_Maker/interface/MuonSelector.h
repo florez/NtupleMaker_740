d123a9c407a5139f4260206e2137d1a438f576ed
Alfredo <Alfredo.Gurrola@cern.ch>
2015-08-19 08:57:00 -0500

    fixed minor bugs, cleaned up code, added additional variables (e.g. lifetime)

--- BSM3G_TNT_Maker/interface/MuonSelector.h
+++ BSM3G_TNT_Maker/interface/MuonSelector.h
@@ -1,7 +1,6 @@
-// 
-//  Authors:  Andres Florez: Universidad de los Andes, Colombia. 
-//  kaur amandeepkalsi: Panjab University, India. 
-// 
+// Authors:  Alfredo Gurrola (Vanderbilt University)
+// Andres Florez: Universidad de los Andes, Colombia.
+// kaur amandeepkalsi: Panjab University, India.
 
 #ifndef __MUON_MU_H_                                                                                                                                  
 #define __MUON_MU_H_
@@ -26,31 +25,43 @@
 // user include files
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/Framework/interface/EventSetup.h"
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "FWCore/Common/interface/TriggerNames.h"
+#include "DataFormats/Candidate/interface/Candidate.h"
+#include "DataFormats/HepMCCandidate/interface/GenParticle.h"
 #include "DataFormats/VertexReco/interface/VertexFwd.h"
 #include "DataFormats/VertexReco/interface/Vertex.h"
 #include "DataFormats/PatCandidates/interface/Muon.h"
 #include "DataFormats/PatCandidates/interface/MET.h"
 #include "DataFormats/PatCandidates/interface/PackedCandidate.h"
-#include "FWCore/ServiceRegistry/interface/Service.h"
-#include "CommonTools/UtilAlgos/interface/TFileService.h"
-#include "CommonTools/Utils/interface/TFileDirectory.h"
-#include "DataFormats/Math/interface/deltaR.h"
-#include "FWCore/Common/interface/TriggerNames.h"
 #include "DataFormats/PatCandidates/interface/TriggerObjectStandAlone.h"
 #include "DataFormats/PatCandidates/interface/PackedTriggerPrescales.h"
-#include "DataFormats/Common/interface/TriggerResults.h"
-#include "HLTrigger/HLTcore/interface/HLTConfigProvider.h"
 #include "DataFormats/PatCandidates/interface/PackedGenParticle.h"
-#include "DataFormats/Candidate/interface/Candidate.h"
-#include "DataFormats/HepMCCandidate/interface/GenParticle.h"
+#include "DataFormats/Common/interface/TriggerResults.h"
+#include "DataFormats/Math/interface/deltaR.h"
+#include "CommonTools/UtilAlgos/interface/TFileService.h"
+#include "CommonTools/Utils/interface/TFileDirectory.h"
+#include "HLTrigger/HLTcore/interface/HLTConfigProvider.h"
+#include "MagneticField/Engine/interface/MagneticField.h"
+#include "MagneticField/Records/interface/IdealMagneticFieldRecord.h"
+#include "Geometry/DTGeometry/interface/DTGeometry.h"
+#include "Geometry/CaloGeometry/interface/CaloGeometry.h"
+#include "Geometry/Records/interface/CaloGeometryRecord.h"
+#include "Geometry/CommonDetUnit/interface/GlobalTrackingGeometry.h"
+#include "TrackingTools/Records/interface/TransientTrackRecord.h"
+#include "TrackingTools/TransientTrack/interface/TransientTrackBuilder.h"
+#include "RecoVertex/KinematicFitPrimitives/interface/KinematicParticleFactoryFromTransientTrack.h"
 #include "baseTree.h"
 
 using namespace std;
 using namespace pat;
 using namespace edm;
+
 //
 // class declaration
 //
@@ -61,7 +72,7 @@ public:
 
   MuonSelector(std::string name, TTree* tree, bool debug, const edm::ParameterSet& cfg);
   ~MuonSelector();
-  void Fill(const edm::Event& iEvent);
+  void Fill(const edm::Event& iEvent, const edm::EventSetup& iSetup);
   void SetBranches();
   void Clear();
   bool isGoodVertex(const reco::Vertex& vtx);
@@ -71,12 +82,19 @@ private:
   // ----------member data ---------------------------
 
   vector <double> Muon_isoSum , Muon_isoCharParPt ;
-  vector <double> Muon_pt ,Muon_eta,Muon_phi, Muon_dz, Muon_energy, Muon_iso;
+  vector <double> Muon_pt ,Muon_eta,Muon_phi, Muon_dz_pv, Muon_energy, Muon_iso;
   vector <double> Muon_isoCharged, Muon_isoNeutralHadron , Muon_isoPhoton, Muon_isoPU;
-  vector <double> Muon_charge, Muon_chi2, Muon_p, Muon_matchedStat, Muon_dxy, Muon_dxy_bs; 
-  vector <double> Muon_dxy_bs_dz, Muon_dzError, Muon_dxyError, Muon_ndof, Muon_vtx, Muon_vty, Muon_vtz; 
+  vector <double> Muon_charge, Muon_chi2, Muon_p, Muon_matchedStat, Muon_dxy_pv, Muon_dxy_bs; 
+  vector <double> Muon_dz_bs, Muon_dzError, Muon_dxyError, Muon_ndof, Muon_vtx, Muon_vty, Muon_vtz; 
   vector <double> Muon_track_pt, Muon_track_ptError, Muon_validHits, Muon_validHitsInner, Muon_TLayers; 
-  vector <bool> Muon_loose, Muon_tight, Muon_soft, Muon_isHighPt, Muon_pf, Muon_isGlobal;   
+  vector <bool> Muon_loose, Muon_medium, Muon_tight, Muon_soft, Muon_isHighPt, Muon_pf, Muon_isGlobal;   
+  vector<double> Muon_dB, Muon_besttrack_pt, Muon_besttrack_ptError, Muon_tunePBestTrack_pt;
+  vector<double> Muon_isTrackerMuon, Muon_POGisGood;
+  vector<double> Muon_chi2LocalPosition, Muon_trkKink, Muon_segmentCompatibility, Muon_validFraction, Muon_pixelLayersWithMeasurement, Muon_qualityhighPurity;
+  vector<double> Muon_tunePBestTrackType;
+  vector<double> Muon_track_PCAx_bs, Muon_track_PCAy_bs, Muon_track_PCAz_bs;
+  vector<double> Muon_track_PCAx_pv, Muon_track_PCAy_pv, Muon_track_PCAz_pv;
+  vector<double> Muon_trackFitErrorMatrix_00, Muon_trackFitErrorMatrix_01, Muon_trackFitErrorMatrix_02, Muon_trackFitErrorMatrix_11, Muon_trackFitErrorMatrix_12, Muon_trackFitErrorMatrix_22;
 
   // super tiny ntuple?
   bool _super_TNT;
@@ -90,6 +108,7 @@ private:
   int _Muon_vtx_ndof_min;
   int _Muon_vtx_rho_max;
   double _Muon_vtx_position_z_max;
+
 };
 
 #endif
