510cad5fc1be821ad6b280b1c66222b8541600bb
gurrola <Alfredo.Gurrola@cern.ch>
2015-08-27 05:33:47 -0500

    fixed bug in gen selector; fixed NULL lead track reference issue in the tau selector

---
+++ BSM3G_TNT_Maker/python/miniAOD_747_50ns.py
@@ -0,0 +1,167 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("Demo")
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+
+process.load('TrackingTools.TransientTrack.TransientTrackBuilder_cfi')
+
+process.load('Configuration.Geometry.GeometryRecoDB_cff')
+#process.load('Configuration.Geometry.GeometryIdeal_cff')
+
+process.load("Configuration.StandardSequences.MagneticField_38T_PostLS1_cff")
+#process.load("Configuration.StandardSequences.MagneticField_cff")
+
+#needs to be commented out or else it complains about: 'two EventSetup Producers want to deliver type="CaloSubdetectorGeometry" label="ZDC"'
+#process.load("Configuration.StandardSequences.Geometry_cff")
+
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.GlobalTag.globaltag = 'MCRUN2_74_V9A::All'
+
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(500) )
+
+process.source = cms.Source("PoolSource",
+  fileNames = cms.untracked.vstring(
+    '/store/mc/RunIISpring15DR74/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/MINIAODSIM/Asympt50ns_MCRUN2_74_V9A-v2/00000/00C4781D-6B08-E511-8A0A-0025905A6084.root'
+  )
+)
+
+#
+# START ELECTRON ID SECTION
+#
+# Set up everything that is needed to compute electron IDs and
+# add the ValueMaps with ID decisions into the event data stream
+#
+# Load tools and function definitions
+from PhysicsTools.SelectorUtils.tools.vid_id_tools import *
+process.load("RecoEgamma.ElectronIdentification.egmGsfElectronIDs_cfi")
+# overwrite a default parameter: for miniAOD, the collection name is a slimmed one
+process.egmGsfElectronIDs.physicsObjectSrc = cms.InputTag('slimmedElectrons')
+from PhysicsTools.SelectorUtils.centralIDRegistry import central_id_registry
+process.egmGsfElectronIDSequence = cms.Sequence(process.egmGsfElectronIDs)
+# Define which IDs we want to produce
+# Each of these two example IDs contains all four standard
+# cut-based ID working points (only two WP of the PU20bx25 are actually used here).
+# define which IDs we want to produce
+my_id_modules = ['RecoEgamma.ElectronIdentification.Identification.cutBasedElectronID_Spring15_50ns_V1_cff',
+                 'RecoEgamma.ElectronIdentification.Identification.heepElectronID_HEEPV60_cff']
+#Add them to the VID producer
+for idmod in my_id_modules:
+    setupAllVIDIdsInModule(process,idmod,setupVIDElectronSelection)
+# Do not forget to add the egmGsfElectronIDSequence to the path,
+# as in the example below!
+#
+# END ELECTRON ID SECTION
+#
+
+process.TFileService = cms.Service("TFileService",
+fileName = cms.string("OutTree.root")
+)
+
+process.TNT = cms.EDAnalyzer("BSM3G_TNT_Maker",
+    # boolean variables
+    debug_ = cms.bool(False),
+    filltriggerinfo     = cms.bool(True),
+    fillmuoninfo        = cms.bool(True),
+    fillelectronpatinfo = cms.bool(True),
+    filltauinfo         = cms.bool(True),
+    fillgeninfo         = cms.bool(True),
+    fillPVinfo          = cms.bool(True),
+    filljetinfo         = cms.bool(True),
+    fillMETinfo         = cms.bool(True),
+    fillphotoninfo      = cms.bool(True),   
+
+    # make a super tiny ntuple, only with a few branches?
+    super_TNT  = cms.bool(False),
+    # is data or MC?
+    is_data     = cms.bool(False),
+ 
+    # input tags 
+    triggerResults      = cms.InputTag( 'TriggerResults', '', 'PAT' ),
+    vertices            = cms.InputTag("offlineSlimmedPrimaryVertices"),
+    beamSpot            = cms.InputTag("offlineBeamSpot"),
+    muons               = cms.InputTag("slimmedMuons"),
+    patElectrons        = cms.InputTag("slimmedElectrons"),
+    taus                = cms.InputTag("slimmedTaus"),
+    photons             = cms.InputTag("slimmedPhotons"),
+    jets                = cms.InputTag("slimmedJets"),
+    jetsPUPPI           = cms.InputTag("slimmedJetsPuppi"),
+    fatjets             = cms.InputTag("slimmedJetsAK8"),
+    mets                = cms.InputTag("slimmedMETs"),
+    metsPUPPI           = cms.InputTag("slimmedMETsPuppi"),
+    bits                = cms.InputTag("TriggerResults","","HLT"),
+    prescales           = cms.InputTag("patTrigger"),
+    objects             = cms.InputTag("selectedPatTrigger"),  
+    packedPFCandidates  = cms.InputTag("packedPFCandidates"),
+    electronVetoIdMap   = cms.InputTag("egmGsfElectronIDs:cutBasedElectronID-Spring15-50ns-V1-standalone-veto"),
+    electronLooseIdMap  = cms.InputTag("egmGsfElectronIDs:cutBasedElectronID-Spring15-50ns-V1-standalone-loose"),
+    electronMediumIdMap = cms.InputTag("egmGsfElectronIDs:cutBasedElectronID-Spring15-50ns-V1-standalone-medium"),
+    electronTightIdMap  = cms.InputTag("egmGsfElectronIDs:cutBasedElectronID-Spring15-50ns-V1-standalone-tight"),
+    eleHEEPIdMap        = cms.InputTag("egmGsfElectronIDs:heepElectronID-HEEPV60"),
+
+    # muon cuts
+    Muon_pt_min              = cms.double(8.0),
+    Muon_eta_max             = cms.double(2.4),
+    Muon_vtx_ndof_min        = cms.int32(4),
+    Muon_vtx_rho_max         = cms.int32(2),
+    Muon_vtx_position_z_max  = cms.double(24.),
+
+    # electron cuts
+    patElectron_pt_min       = cms.double(10.0),
+    patElectron_eta_max      = cms.double(2.4),
+    patElectron_vtx_ndof_min        = cms.int32(4),
+    patElectron_vtx_rho_max         = cms.int32(2),
+    patElectron_vtx_position_z_max  = cms.double(24.),
+
+    # tau cuts
+    Tau_pt_min  = cms.double(20.),
+    Tau_eta_max = cms.double(2.3),
+    Tau_vtx_ndof_min        = cms.int32(4),
+    Tau_vtx_rho_max         = cms.int32(2),
+    Tau_vtx_position_z_max  = cms.double(24.),
+
+    # jet cuts
+    Jet_pt_min   = cms.double(30.),
+
+    # photon cuts 
+    Photon_pt_min   = cms.double(10.0),
+    Photon_eta_max  = cms.double(5.0),    
+
+    # primary vertex cuts
+    Pvtx_ndof_min   = cms.double(4.),
+    Pvtx_vtx_max  = cms.double(24.),
+    Pvtx_vtxdxy_max = cms.double(24.),
+
+)
+
+# filter out anomalous MET from detector noise, cosmic rays, and beam halo particles
+process.load("RecoMET.METFilters.metFilters_cff")
+
+process.primaryVertexFilter = cms.EDFilter("GoodVertexFilter",
+                                           vertexCollection = cms.InputTag('offlineSlimmedPrimaryVertices'),
+                                           minimumNDOF = cms.uint32(4) ,
+                                           maxAbsZ = cms.double(24),
+                                           maxd0 = cms.double(2)
+)
+
+##___________________________HCAL_Noise_Filter________________________________||
+process.load('CommonTools.RecoAlgos.HBHENoiseFilterResultProducer_cfi')
+process.HBHENoiseFilterResultProducer.minZeros = cms.int32(99999)
+
+process.ApplyBaselineHBHENoiseFilter = cms.EDFilter('BooleanFlagFilter',
+   inputLabel = cms.InputTag('HBHENoiseFilterResultProducer','HBHENoiseFilterResult'),
+   reverseDecision = cms.bool(False)
+)
+
+process.p = cms.Path(process.primaryVertexFilter * 
+#                     process.CSCTightHaloFilter * 
+#                     process.eeBadScFilter * 
+#                     process.HBHENoiseFilterResultProducer * 
+#                     process.ApplyBaselineHBHENoiseFilter * 
+                     process.egmGsfElectronIDSequence * 
+                     process.TNT
+)
+#process.p = cms.Path(process.TNT)
